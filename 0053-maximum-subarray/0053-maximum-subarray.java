34d3451cea0233d78f32197ae5896585e03bf246
jashwanth jampala <110185187+Jashwanth43@users.noreply.github.com>
2025-03-15 19:46:06 -0700

    Time: 1 ms (99.5%), Space: 57 MB (56.29%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,7 +3,7 @@ class Solution {
         int sum=0;
         int max=nums[0];
         for(int i=0;i<nums.length;i++){
-            sum=sum+nums[i];
+            sum+=nums[i];
             max=Math.max(sum,max);
             if(sum<0){
                 sum=0;
