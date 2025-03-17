c57de93bd9d8db8ba0ff054867937a641f641eb9
jashwanth jampala <110185187+Jashwanth43@users.noreply.github.com>
2025-03-16 22:27:48 -0700

    Time: 14 ms (33.98%), Space: 44.7 MB (44.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,14 +4,24 @@ class Solution {
         if(s.length()<=0) return true;
         s=s.toLowerCase();
         
-        StringBuffer sb=new StringBuffer();
-        for(int i=s.length()-1;i>=0;i--){
-         sb.append(s.charAt(i));
-        }
-        String res=sb.toString();
+        // StringBuffer sb=new StringBuffer();
+        // for(int i=s.length()-1;i>=0;i--){
+        //  sb.append(s.charAt(i));
+        // }
+        // String res=sb.toString();
 
-        return s.equals(res);
-        
+        // return s.equals(res);
+        int length=s.length();
+        int start=0;
+        int end=length-1;
+        while(start<end){
+           if(s.charAt(start)!=s.charAt(end)){
+            return false;
+           }
+           start++;
+           end--;
+        }
+        return true;
 
     }
 }
