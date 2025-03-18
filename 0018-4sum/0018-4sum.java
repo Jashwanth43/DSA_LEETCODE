4fa90ae46128c5bffbb945507386165efc1671ba
Jashwanth Jampala <110185187+Jashwanth43@users.noreply.github.com>
2025-03-18 05:06:39 -0700

    Time: 15 ms (88.64%), Space: 43.6 MB (95.89%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,3 @@
-// class Solution {
-//     public List<List<Integer>> fourSum(int[] nums, int target) {
-//         List<List<Integer>> ls=new ArrayList<>();
-//         Arrays.sort(nums);
-//         for(int i=0;i<nums.length-2;i++){
-//             if(i>0 && nums[i]==nums[i-1])continue;
-//             for(int j=i+1;j<nums.length-1;j++){
-//                 if(j>i+1 && nums[j]==nums[j-1])continue;
-//                 int k=j+1;
-//                 int l=nums.length-1;
-//                 while(k<l){
-//                     int sum=nums[i]+nums[j]+nums[k]+nums[l];
-//                     if(sum >target) l--;
-//                     else if (sum<target) k++;
-//                     else{
-//                         ls.add(List.of(nums[i],nums[j],nums[k],nums[l]));
-//                         k++;
-//                         l--;
-//                         while(k<l && nums[k]==nums[k-1]) k++;
-//                     }
-//                 }
-//             }
-//         }
-//         return ls;
-//     }
-// }
-
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ls = new ArrayList<>();
