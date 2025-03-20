0630158907dc116818bc48b9e3bf6ea1c79c2d13
Jashwanth Jampala <110185187+Jashwanth43@users.noreply.github.com>
2025-03-20 01:31:07 -0700

    Time: 1 ms (97.84%), Space: 45.6 MB (59.73%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -20,6 +20,7 @@ class Solution {
             if((flowerbed[i]==0 && flowerbed[i-1]==0) && (flowerbed[i]==0 && flowerbed[i+1]==0)){
                 flowerbed[i]=1;
                 n--;
+                if(n==0) return true;
             }
         }
 
